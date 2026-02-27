179634e9b52f72848d1e73bdea118f6e027969fe
Xiaoyiawa <1079802404@qq.com>
2026-02-27 22:28:29 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 make clean
 
-export TOOLCHAIN=$HOME/toolchain
+export TOOLCHAIN=/home/xiaoyi/gcc
 export PATH=$PATH:$TOOLCHAIN/bin
-export CC=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-gcc
-export CXX=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-g++
-export AR=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-ar
-export RANLIB=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-ranlib
-export STRIP=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-strip
+export CC=$TOOLCHAIN/bin/arm-unknown-linux-musleabihf-gcc
+export CXX=$TOOLCHAIN/bin/arm-unknown-linux-musleabihf-g++
+export AR=$TOOLCHAIN/bin/arm-unknown-linux-musleabihf-ar
+export RANLIB=$TOOLCHAIN/bin/arm-unknown-linux-musleabihf-ranlib
+export STRIP=$TOOLCHAIN/bin/arm-unknown-linux-musleabihf-strip
 
 export STAGING_DIR=$TOOLCHAIN/bin
 
